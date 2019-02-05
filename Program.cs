338315a4bfe23f9a84166178678cdca8ec78e065
Omar Ahmad <omarahmad293@gmail.com>
2019-02-05 22:27:52 +0200

    Updated assembler
    works well except the J_Type instructions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace testMIPS
 {
@@ -10,24 +10,43 @@ namespace testMIPS
 			"s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7" ,
 			"t8", "t9", "k0", "k1", "gp", "sp", "fp", "ra"};
 
+		readonly static String[] R_Type_instructions = { "ADD", "AND", "OR", "JR", "SLL", "SLT" };
+		readonly static String[] I_Type_instructions = { "LW", "SW", "BEQ", "ADDI", "ORI" };
+		readonly static String[] J_Type_instructions = { "J", "JAL" };
+
 		static void Main(string[] args)
 		{
-			String instruction = "SLL $S1, $S2, 15";
+			String instruction = "J LOOP";
+			//"SLL $S1, $S2, 15";
 			//"LW $S2, 50($S1)";
-			//"ADD $R1, $R2, $R3";
+			//"ADD $S1, $S2, $S3";
 
 			String[] words = SplitInstruction(instruction);
 
-			if (instruction.IndexOf('(') == -1) // no brackets
+			if (Array.Exists(R_Type_instructions, x => x.Equals(words[0])))
 			{
-				if (words.Length == 4) //R-Type
-				{
-					Console.WriteLine(instruction);
-					Console.WriteLine("oooooossssstttttdddddhhhhhffffff");
-					Console.WriteLine(R_Type(words));
-					Console.WriteLine(R_Type(words).Length);
-				}
+				Console.WriteLine(instruction);
+				Console.WriteLine("oooooossssstttttdddddhhhhhffffff");
+				Console.WriteLine(R_Type(words));
+				Console.WriteLine(R_Type(words).Length);
 			}
+
+			if (Array.Exists(I_Type_instructions, x => x.Equals(words[0])))
+			{
+				Console.WriteLine(instruction);
+				Console.WriteLine("oooooossssstttttffffffffffffffff");
+				Console.WriteLine(I_Type(words));
+				Console.WriteLine(I_Type(words).Length);
+			}
+
+			if (Array.Exists(J_Type_instructions, x => x.Equals(words[0])))
+			{
+				Console.WriteLine(instruction);
+				Console.WriteLine("ooooooaaaaaaaaaaaaaaaaaaaaaaaaaa");
+				Console.WriteLine(J_Type(words));
+				Console.WriteLine(J_Type(words).Length);
+			}
+
 		}
 
 		static String[] SplitInstruction(String instruction)
@@ -43,9 +62,18 @@ namespace testMIPS
 		static String R_Type(String[] array)
 		{
 			String opCode = "000000";
-			String d = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[1].ToLower())), 2).PadLeft(5, '0');
-			String s = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[2].ToLower())), 2).PadLeft(5, '0');
-			String t = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[3].ToLower())), 2).PadLeft(5, '0');
+			String s = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[1].ToLower())), 2).PadLeft(5, '0');
+			String d, t;
+			if (array.Length > 2)
+			{
+				d = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[2].ToLower())), 2).PadLeft(5, '0');
+				t = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[3].ToLower())), 2).PadLeft(5, '0');
+			}
+			else
+			{
+				d = "00000";
+				t = "00000";
+			}
 			String shamt = "00000";
 			String function = "";
 
@@ -72,10 +100,84 @@ namespace testMIPS
 				case "SLT":
 					function = "101010";
 					break;
+
+				case "JR":
+					function = "001000";
+					t = "00000";
+					d = "00000";
+					break;
 			}
 
 			return opCode + s + t + d + shamt + function;
 		}
 
+		static String I_Type(String[] array)
+		{
+			String opCode = "";
+			String s = "";
+			String t = "";
+			String immediate = "";
+
+			if (array.Length == 3) //LW or SW
+			{
+				t = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[1].ToLower())), 2).PadLeft(5, '0');
+				String rs = array[2].Substring(array[2].IndexOf('$') + 1).Trim(')');
+				s = Convert.ToString(Array.FindIndex(registers, x => x.Equals(rs.ToLower())), 2).PadLeft(5, '0');
+				immediate = Convert.ToString(Int16.Parse(array[2].Substring(0, array[2].IndexOf('('))), 2).PadLeft(16, '0');
+
+				switch (array[0])
+				{
+					case "LW":
+						opCode = "100011";
+						break;
+
+					case "SW":
+						opCode = "101011";
+						break;
+				}
+			}
+			else
+			{
+				s = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[2].ToLower())), 2).PadLeft(5, '0');
+				t = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[1].ToLower())), 2).PadLeft(5, '0');
+				immediate = Convert.ToString(Int16.Parse(array[3]), 2).PadLeft(16, '0');
+				switch (array[0])
+				{
+					case "ADDI":
+						opCode = "001000";
+						break;
+
+					case "ORI":
+						opCode = "001101";
+						break;
+
+					case "BEQ":
+						opCode = "000100";
+						s = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[1].ToLower())), 2).PadLeft(5, '0');
+						t = Convert.ToString(Array.FindIndex(registers, x => x.Equals(array[2].ToLower())), 2).PadLeft(5, '0');
+						break;
+				}
+			}
+			return opCode + s + t + immediate;
+		}
+
+		static String J_Type(String[] array)
+		{
+			String opCode = "";
+			String immediate = array[1]; //TODO: Calculate offset between next instruction and label and convert to binary
+
+			switch (array[0])
+			{
+				case "J":
+					opCode = "000010";
+					break;
+
+				case "JAL":
+					opCode = "000011";
+					break;
+			}
+
+			return opCode + immediate;
+		}
 	}
 }
